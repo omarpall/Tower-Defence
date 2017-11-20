a8e904bdbfdf4d5ad827bdfaf703ade85ffab561
Guðmundur Orri Pálsson <gudmundurorri4@gmail.com>
2017-11-20 14:13:55 +0000

    g

--- Sprite.js
+++ Sprite.js
@@ -45,6 +45,30 @@ Sprite.prototype.drawCentredAt = function (ctx, cx, cy, rotation) {
     ctx.restore();
 };
 
+Sprite.prototype.drawAnimatedAt = function(ctx, cx, cy, rotation) {
+  if (rotation === undefined) rotation = 0;
+
+  var w = this.width,
+      h = this.height;
+
+  ctx.save();
+  ctx.translate(cx, cy);
+  ctx.rotate(rotation);
+  ctx.scale(this.scale, this.scale);;
+
+  // drawImage expects "top-left" coords, so we offset our destination
+  // coords accordingly, to draw our sprite centred at the origin
+  if(start === 1){
+    ctx.drawImage(this.image,
+                  -w/2, -h/2);
+  }
+  if(start === 2){
+    ctx.drawImage(this.image,
+                  -w/2, -h/2);
+  }
+  ctx.restore();
+}
+
 Sprite.prototype.drawWrappedCentredAt = function (ctx, cx, cy, rotation) {
 
     // Get "screen width"
